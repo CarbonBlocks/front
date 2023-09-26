a9336398688883c579f6c6aaf5237a445ea262cf
Aaron Kigler <aaronkigler@gmail.com>
2023-09-26 07:39:49 -0400

    frontend

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,15 +1,30 @@
-import VideoThumb from '@/public/images/hero-image.png'
-import ModalVideo from '@/components/modal-video'
-
+"use client";
+import VideoThumb from "@/public/images/hero-image.png";
+import ModalVideo from "@/components/modal-video";
+import SignUp from "./SignUp";
+//#35363B
 export default function Hero() {
   return (
-    <section className="relative">
-
+    <section className="relative bg-[#1F252D]">
       {/* Illustration behind hero content */}
-      <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none -z-1" aria-hidden="true">
-        <svg width="1360" height="578" viewBox="0 0 1360 578" xmlns="http://www.w3.org/2000/svg">
+      {/* <div
+        className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none -z-1"
+        aria-hidden="true"
+      >
+        <svg
+          width="1360"
+          height="578"
+          viewBox="0 0 1360 578"
+          xmlns="http://www.w3.org/2000/svg"
+        >
           <defs>
-            <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="illustration-01">
+            <linearGradient
+              x1="50%"
+              y1="0%"
+              x2="50%"
+              y2="100%"
+              id="illustration-01"
+            >
               <stop stopColor="#FFF" offset="0%" />
               <stop stopColor="#EAEAEA" offset="77.402%" />
               <stop stopColor="#DFDFDF" offset="100%" />
@@ -20,42 +35,149 @@ export default function Hero() {
             <circle cx="155" cy="443" r="64" />
           </g>
         </svg>
+      </div> */}
+      {/* <div
+        className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none -z-1"
+        aria-hidden="true"
+      > */}
+      <div className="absolute top-[20%] left-[1.25vh] md:top-[20%] md:right-10 md:left-auto">
+        <svg
+          id="eZlVwhPziUQ1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 659 719"
+          shapeRendering="geometricPrecision"
+          textRendering="geometricPrecision"
+          height="60vh"
+        >
+          <defs>
+            <linearGradient
+              id="eZlVwhPziUQ2-fill"
+              x1="329.5"
+              y1="-20"
+              x2="329.5"
+              y2="739"
+              spreadMethod="pad"
+              gradientUnits="userSpaceOnUse"
+              gradientTransform="translate(0 0)"
+            >
+              <stop id="eZlVwhPziUQ2-fill-0" offset="0%" stopColor="#1f87e7" />
+              <stop
+                id="eZlVwhPziUQ2-fill-1"
+                offset="69.7917%"
+                stopColor="rgba(25,91,153,0.327394)"
+              />
+              <stop
+                id="eZlVwhPziUQ2-fill-2"
+                offset="79.1667%"
+                stopColor="rgba(24,85,142,0.237044)"
+              />
+              <stop
+                id="eZlVwhPziUQ2-fill-3"
+                offset="96.875%"
+                stopColor="rgba(22,74,122,0.0663823)"
+              />
+              <stop
+                id="eZlVwhPziUQ2-fill-4"
+                offset="99.97%"
+                stopColor="rgba(27,107,181,0.568327)"
+              />
+              <stop
+                id="eZlVwhPziUQ2-fill-5"
+                offset="99.98%"
+                stopColor="rgba(22,72,119,0.0364583)"
+              />
+              <stop
+                id="eZlVwhPziUQ2-fill-6"
+                offset="99.99%"
+                stopColor="rgba(22,74,121,0.0572917)"
+              />
+              <stop
+                id="eZlVwhPziUQ2-fill-7"
+                offset="100%"
+                stopColor="rgba(22,70,115,0)"
+              />
+            </linearGradient>
+          </defs>
+          <path
+            d="M263.5,18.1051c40.841-23.57951,91.159-23.57951,132,0L592.157,131.645c40.841,23.579,66,67.156,66,114.315v227.08c0,47.159-25.159,90.736-66,114.315L395.5,700.895c-40.841,23.579-91.159,23.579-132,0L66.8433,587.355C26.0024,563.776,0.843353,520.199,0.843353,473.04v-227.08c0-47.159,25.159047-90.736,66.000047-114.315L263.5,18.1051Z"
+            fill="url(#eZlVwhPziUQ2-fill)"
+          />
+          <rect
+            mix-blend-mode="overlay"
+            width="295"
+            height="60"
+            rx="30"
+            ry="30"
+            transform="translate(182 329.499846)"
+            fillOpacity="0.3"
+            strokeWidth="0"
+          />
+        </svg>
       </div>
 
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
-
         {/* Hero content */}
-        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-
+        {/* <div className="pt-32 pb-12 md:pt-60 md:pb-20"> */}
+        <div className="pt-48 md:pt-60 md:pb-20">
           {/* Section header */}
-          <div className="text-center pb-12 md:pb-16">
-            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Make your website <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">wonderful</span></h1>
-            <div className="max-w-3xl mx-auto">
-              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.</p>
-              <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
-                <div>
-                  <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Start free trial</a>
+          {/* <div className=" pb-12 md:pb-16"> */}
+          <div className="md:pb-8">
+            <h1
+              className="text-5xl text-white md:text-6xl font-extrabold leading-tighter tracking-tighter mb-5"
+              data-aos="zoom-y-out"
+            >
+              Building the carbon economy
+            </h1>
+            <div className="max-w-3xl mo mt-10">
+              <p
+                className="text-xl font-extrabold text-white mb-8"
+                data-aos="zoom-y-out"
+                data-aos-delay="150"
+              >
+                By employing existing cashback marketing, Carbon Blocks
+                seamlessly allocates a portion of everyday transactions to help
+                businesses achieve their net-zero emissions targets while
+                empowering consumers to take meaningful action against climate
+                change.
+              </p>
+              <div
+                className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center"
+                data-aos="zoom-y-out"
+                data-aos-delay="300"
+              >
+                <SignUp />
+                {/* <div>
+                  <a
+                    className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0"
+                    href="#0"
+                  >
+                    Start free trial
+                  </a>
                 </div>
                 <div>
-                  <a className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Learn more</a>
-                </div>
+                  <a
+                    className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4"
+                    href="#0"
+                  >
+                    Learn more
+                  </a>
+                </div> */}
               </div>
             </div>
           </div>
 
           {/* Hero image */}
-          <ModalVideo
+          {/* <ModalVideo
             thumb={VideoThumb}
             thumbWidth={768}
             thumbHeight={432}
             thumbAlt="Modal video thumbnail"
             video="/videos/video.mp4"
             videoWidth={1920}
-            videoHeight={1080} />
-
+            videoHeight={1080}
+          /> */}
         </div>
-
       </div>
     </section>
-  )
+  );
 }
--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -1,18 +1,68 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Logo() {
   return (
     <Link href="/" className="block" aria-label="Cruip">
-      <svg className="w-8 h-8" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+      <svg
+        width="50"
+        height="50"
+        viewBox="0 0 2000 2000"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle cx="1000" cy="1000" r="1000" fill="#1F87E7" />
+        <g filter="url(#filter0_d_1340_2)">
+          <path
+            d="M900.5 239.946C962.071 204.398 1037.93 204.398 1099.5 239.946L1608.48 533.804C1670.05 569.352 1707.98 635.047 1707.98 706.143V1293.86C1707.98 1364.95 1670.05 1430.65 1608.48 1466.2L1099.5 1760.05C1037.93 1795.6 962.071 1795.6 900.5 1760.05L391.524 1466.2C329.953 1430.65 292.024 1364.95 292.024 1293.86V706.143C292.024 635.047 329.953 569.352 391.524 533.804L900.5 239.946Z"
+            fill="black"
+          />
+        </g>
+        <rect
+          x="630"
+          y="965"
+          width="737.5"
+          height="150"
+          rx="75"
+          fill="#1F87E7"
+        />
         <defs>
-          <radialGradient cx="21.152%" cy="86.063%" fx="21.152%" fy="86.063%" r="79.941%" id="footer-logo">
-            <stop stopColor="#4FD1C5" offset="0%" />
-            <stop stopColor="#81E6D9" offset="25.871%" />
-            <stop stopColor="#338CF5" offset="100%" />
-          </radialGradient>
+          <filter
+            id="filter0_d_1340_2"
+            x="288.024"
+            y="213.286"
+            width="1423.95"
+            height="1581.43"
+            filterUnits="userSpaceOnUse"
+            color-interpolation-filters="sRGB"
+          >
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feColorMatrix
+              in="SourceAlpha"
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+            />
+            <feOffset dy="4" />
+            <feGaussianBlur stdDeviation="2" />
+            <feComposite in2="hardAlpha" operator="out" />
+            <feColorMatrix
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+            />
+            <feBlend
+              mode="normal"
+              in2="BackgroundImageFix"
+              result="effect1_dropShadow_1340_2"
+            />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="effect1_dropShadow_1340_2"
+              result="shape"
+            />
+          </filter>
         </defs>
-        <rect width="32" height="32" rx="16" fill="url(#footer-logo)" fillRule="nonzero" />
       </svg>
     </Link>
-  )
+  );
 }
