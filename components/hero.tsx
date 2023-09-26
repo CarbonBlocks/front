25760dda8e5f03c6ddc0e1c4ee62f0faaa16153c
Aaron Kigler <aaronkigler@gmail.com>
2023-09-26 10:04:02 -0400

    fixed background svg

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,7 +1,10 @@
 "use client";
 import VideoThumb from "@/public/images/hero-image.png";
+import CarbSvg from "@/public/images/carb.svg";
 import ModalVideo from "@/components/modal-video";
 import SignUp from "./SignUp";
+import Image from "next/image";
+
 //#35363B
 export default function Hero() {
   return (
@@ -40,83 +43,93 @@ export default function Hero() {
         className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none -z-1"
         aria-hidden="true"
       > */}
-      {/* <div className="absolute top-[20%] left-[1.25vh] md:top-[20%] md:right-10 md:left-auto"> */}
-      <div className="absolute top-[15%] left-[5%] md:top-[20%] md:right-10 md:left-auto">
-        <svg
-          id="eZlVwhPziUQ1"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 659 719"
-          shapeRendering="geometricPrecision"
-          textRendering="geometricPrecision"
-          height="60vh"
-        >
-          <defs>
-            <linearGradient
-              id="eZlVwhPziUQ2-fill"
-              x1="329.5"
-              y1="-20"
-              x2="329.5"
-              y2="739"
-              spreadMethod="pad"
-              gradientUnits="userSpaceOnUse"
-              gradientTransform="translate(0 0)"
-            >
-              <stop id="eZlVwhPziUQ2-fill-0" offset="0%" stopColor="#1f87e7" />
-              <stop
-                id="eZlVwhPziUQ2-fill-1"
-                offset="69.7917%"
-                stopColor="rgba(25,91,153,0.327394)"
-              />
-              <stop
-                id="eZlVwhPziUQ2-fill-2"
-                offset="79.1667%"
-                stopColor="rgba(24,85,142,0.237044)"
-              />
-              <stop
-                id="eZlVwhPziUQ2-fill-3"
-                offset="96.875%"
-                stopColor="rgba(22,74,122,0.0663823)"
-              />
-              <stop
-                id="eZlVwhPziUQ2-fill-4"
-                offset="99.97%"
-                stopColor="rgba(27,107,181,0.568327)"
-              />
-              <stop
-                id="eZlVwhPziUQ2-fill-5"
-                offset="99.98%"
-                stopColor="rgba(22,72,119,0.0364583)"
-              />
-              <stop
-                id="eZlVwhPziUQ2-fill-6"
-                offset="99.99%"
-                stopColor="rgba(22,74,121,0.0572917)"
-              />
-              <stop
-                id="eZlVwhPziUQ2-fill-7"
-                offset="100%"
-                stopColor="rgba(22,70,115,0)"
-              />
-            </linearGradient>
-          </defs>
-          <path
-            d="M263.5,18.1051c40.841-23.57951,91.159-23.57951,132,0L592.157,131.645c40.841,23.579,66,67.156,66,114.315v227.08c0,47.159-25.159,90.736-66,114.315L395.5,700.895c-40.841,23.579-91.159,23.579-132,0L66.8433,587.355C26.0024,563.776,0.843353,520.199,0.843353,473.04v-227.08c0-47.159,25.159047-90.736,66.000047-114.315L263.5,18.1051Z"
-            fill="url(#eZlVwhPziUQ2-fill)"
-          />
-          <rect
-            mix-blend-mode="overlay"
-            width="295"
-            height="60"
-            rx="30"
-            ry="30"
-            transform="translate(182 329.499846)"
-            fillOpacity="0.3"
-            strokeWidth="0"
-          />
-        </svg>
+      {/* <div className="absolute  top-[20%] left-[1.25vh] md:top-[20%] md:right-10 md:left-auto z-10"> */}
+      {/* <div className="absolute top-[20%] left-[0%] md:top-[20%] md:right-10 md:left-auto"> */}
+      {/* <div className="h-[30vh] md:h-[60vh] absolute top-[20%] left-[1.25vh] md:top-[20%] md:right-10 md:left-auto"> */}
+      {/* <div className="absolute z-20 top-10 left-4 md:top-20 md:right-10 md:left-auto"> */}
+      <div className="absolute top-0 left-0 w-full h-full z-0">
+        <div className="absolute top-[20%] left-[1.25vh] md:top-[20%] md:right-10 md:left-auto">
+          <svg
+            id="eZlVwhPziUQ1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 659 719"
+            shapeRendering="geometricPrecision"
+            textRendering="geometricPrecision"
+            height="60vh"
+          >
+            <defs>
+              <linearGradient
+                id="eZlVwhPziUQ2-fill"
+                x1="329.5"
+                y1="-20"
+                x2="329.5"
+                y2="739"
+                spreadMethod="pad"
+                gradientUnits="userSpaceOnUse"
+                gradientTransform="translate(0 0)"
+              >
+                <stop
+                  id="eZlVwhPziUQ2-fill-0"
+                  offset="0%"
+                  stopColor="#1f87e7"
+                />
+                <stop
+                  id="eZlVwhPziUQ2-fill-1"
+                  offset="69.7917%"
+                  stopColor="rgba(25,91,153,0.327394)"
+                />
+                <stop
+                  id="eZlVwhPziUQ2-fill-2"
+                  offset="79.1667%"
+                  stopColor="rgba(24,85,142,0.237044)"
+                />
+                <stop
+                  id="eZlVwhPziUQ2-fill-3"
+                  offset="96.875%"
+                  stopColor="rgba(22,74,122,0.0663823)"
+                />
+                <stop
+                  id="eZlVwhPziUQ2-fill-4"
+                  offset="99.97%"
+                  stopColor="rgba(27,107,181,0.568327)"
+                />
+                <stop
+                  id="eZlVwhPziUQ2-fill-5"
+                  offset="99.98%"
+                  stopColor="rgba(22,72,119,0.0364583)"
+                />
+                <stop
+                  id="eZlVwhPziUQ2-fill-6"
+                  offset="99.99%"
+                  stopColor="rgba(22,74,121,0.0572917)"
+                />
+                <stop
+                  id="eZlVwhPziUQ2-fill-7"
+                  offset="100%"
+                  stopColor="rgba(22,70,115,0)"
+                />
+              </linearGradient>
+            </defs>
+            <path
+              d="M263.5,18.1051c40.841-23.57951,91.159-23.57951,132,0L592.157,131.645c40.841,23.579,66,67.156,66,114.315v227.08c0,47.159-25.159,90.736-66,114.315L395.5,700.895c-40.841,23.579-91.159,23.579-132,0L66.8433,587.355C26.0024,563.776,0.843353,520.199,0.843353,473.04v-227.08c0-47.159,25.159047-90.736,66.000047-114.315L263.5,18.1051Z"
+              fill="url(#eZlVwhPziUQ2-fill)"
+            />
+            <rect
+              mix-blend-mode="overlay"
+              width="295"
+              height="60"
+              rx="30"
+              ry="30"
+              transform="translate(182 329.499846)"
+              fillOpacity="0.3"
+              strokeWidth="0"
+            />
+          </svg>
+        </div>
       </div>
-
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+      {/* <div className="z-30 max-w-6xl mx-auto px-4 sm:px-6"> */}
+      <div className="relative z-10 max-w-6xl mx-auto px-4 sm:px-6">
+        {/* <div className="absolute z-30 top-1/4 left-1/5 transform -translate-x-1/2 max-w-6xl mx-auto px-4 sm:px-6"> */}
         {/* Hero content */}
         {/* <div className="pt-32 pb-12 md:pt-60 md:pb-20"> */}
         <div className="pt-48 md:pt-60 md:pb-20">
